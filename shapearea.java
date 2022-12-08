c12d64b92dbb41d807803b84f5e31a7531354f09
vibhahugar <117517944+vibhahugar@users.noreply.github.com>
2022-12-08 00:55:29 -0800

    Create shapearea.java
    week 4 lab program
    
    Develop a Java program to create an abstract class named Shape that contains
    two integers and an empty method named printArea( ). Provide three classes
    named Rectangle, Triangle and Circle such that each one of the classes
    extends the class Shape. Each one of the classes contain only the method
    printArea( ) that prints the area of the given shape.

---
+++ shapearea.java
@@ -0,0 +1,60 @@
+abstract class shape {
+double a,b;
+
+shape(int x, int y)
+{
+  a=x;
+  b=y;
+}
+
+abstract void printArea();
+}
+class rectangle extends shape{
+rectangle(int x, int y)
+{
+super(x,y);
+}
+void printArea()
+{
+  System.out.println("Area of rectangle is" +(a*b));
+}
+}
+
+class triangle extends shape{
+triangle(int x, int y)
+{
+super(x,y);
+}
+void printArea()
+{
+  System.out.println("Area of triangle is" +(0.5*a*b));
+}
+}
+
+class circle extends shape{
+circle(int x, int y)
+{
+super(x,y);
+}
+void printArea()
+{
+  System.out.println("Area of circle is" +(3.14*a*a));
+}
+}
+
+class shapearea{
+public static void main(String args[])
+{
+ rectangle r1= new rectangle(10,20);
+ triangle t1= new triangle(5,10);
+ circle c1= new circle(3,0);
+
+  shape r;
+  r=r1;
+  r.printArea();
+r=t1;
+  r.printArea();
+r=c1;
+  r.printArea();
+}
+}
